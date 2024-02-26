de285974adf461fcc9c898a95a304e36441d59d5
Shabab <s7abab2k1@gmail.com>
2024-02-26 23:55:11 +0530

    add jwt authorization

---
+++ server/middlewares/isAuth.js
@@ -0,0 +1,29 @@
+const JWT = require("jsonwebtoken");
+const userModel = require("../models/user.model");
+//token verify
+const isAuth = async (req, res, next) => {
+  try {
+      console.log(req.headers)
+    const decode = JWT.verify(
+      req.headers.authorization,
+      process.env.JWT_SECRET
+    );
+    console.log(decode)
+    const user = await userModel.findById(decode.id);
+    if (!user) {
+      return res.status(401).send({
+        success: false,
+        message: "Unauthorized Access",
+      });
+    }
+    next();
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({
+        success:false,
+        message:'Internal server error'
+    })
+  }
+};
+
+module.exports = { isAuth };
--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const { register, login } = require("../controllers/user.controller");
+const { isAuth } = require("../middlewares/isAuth");
 
 const router = express.Router();
 
 // register
 router.route("/register").post(register);
-router.route("/login").post(login);
+router.route("/login").post(isAuth,login);
 
 module.exports = router;
